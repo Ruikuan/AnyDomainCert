83d45ffbbc11622d51a19371828d035ffbffa708
Ruikuan Li <rli@cn.roblox.com>
2022-07-13 16:04:02 +0800

    Add project files.

---
+++ AnyDomain/Program.cs
@@ -0,0 +1,68 @@
+using System.Collections.Concurrent;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+ConcurrentDictionary<string, Lazy<X509Certificate2>> _certificates = new ConcurrentDictionary<string, Lazy<X509Certificate2>>();
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ConfigureHttpsDefaults(httpsOptions =>
+    {
+        httpsOptions.ServerCertificateSelector = (context, subjectName) =>
+        {
+            Console.WriteLine(subjectName);
+            try
+            {
+                return _certificates.GetOrAdd(subjectName!, (domain) => new Lazy<X509Certificate2>(()=>
+                { 
+
+                    var rootCertPath = Path.Combine("../RootCert", "root.pem");
+                    var rootKeyPath = Path.Combine("../RootCert", "key.pem");
+                    var rootCert = X509Certificate2.CreateFromPemFile(rootCertPath, rootKeyPath);
+
+                    X500DistinguishedNameBuilder dnBuilder = new X500DistinguishedNameBuilder();
+                    dnBuilder.AddCommonName(domain!);
+                    
+                    using var rsa = RSA.Create();
+
+                    CertificateRequest request = new CertificateRequest(dnBuilder.Build(), rsa, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+                    request.CertificateExtensions.Add(new X509BasicConstraintsExtension(false, false, 0, false));
+                    request.CertificateExtensions.Add(new X509KeyUsageExtension(
+                            X509KeyUsageFlags.DigitalSignature | X509KeyUsageFlags.NonRepudiation | X509KeyUsageFlags.KeyEncipherment,
+                            false));
+
+                    request.CertificateExtensions.Add(new X509EnhancedKeyUsageExtension(
+                            new OidCollection
+                            {
+                                new Oid("1.3.6.1.5.5.7.3.1"),
+                                new Oid("1.3.6.1.5.5.7.3.2")
+                            },
+                            true));
+
+                    request.CertificateExtensions.Add(new X509SubjectKeyIdentifierExtension(request.PublicKey, false));
+
+                    Span<byte> bytes = stackalloc byte[4];
+                    Random.Shared.NextBytes(bytes);
+                    var cert = request.Create(rootCert, DateTimeOffset.Now, DateTimeOffset.Now.AddYears(1), bytes);
+                    var certWithKey = cert.CopyWithPrivateKey(rsa);
+                    File.WriteAllBytes($"{domain}.cer", cert.Export(X509ContentType.Cert));
+                    File.WriteAllBytes($"{domain}.pfx", cert.Export(X509ContentType.Pfx));
+                    return certWithKey;
+                })).Value;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        };
+    });
+});
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
---
+++ SelfSignedCertificateGenerator/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+using var rsa = RSA.Create();
+X500DistinguishedNameBuilder dnBuilder = new X500DistinguishedNameBuilder();
+dnBuilder.AddCommonName("hejicho Test Environment Certificate Root");
+CertificateRequest request = new CertificateRequest(dnBuilder.Build(), rsa, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+request.CertificateExtensions.Add(new X509BasicConstraintsExtension(true, false, 0, true));
+request.CertificateExtensions.Add(new X509SubjectKeyIdentifierExtension(request.PublicKey, false));
+var rootCert = request.CreateSelfSigned(DateTimeOffset.Now, DateTimeOffset.Now.AddYears(20));
+//var rootCertWithKey = rootCert.CopyWithPrivateKey(rsa);
+var certPem = rootCert.ExportCertificatePem();
+File.WriteAllText("root.pem", certPem);
+var keyPem = rootCert.GetRSAPrivateKey()!.ExportRSAPrivateKeyPem();
+File.WriteAllText("key.pem", keyPem);
+File.WriteAllBytes("root.cer", rootCert.Export(X509ContentType.Cert));
+File.WriteAllBytes("root.pfx", rootCert.Export(X509ContentType.Pfx));
